2000bff7b45a634d349d7817e625efb660b0b711
Diego <diegohenrique123012@gmail.com>
2025-05-06 22:24:53 -0300

    :sparkles: feat: Insertion of new products into the project

--- landpage/src/app/components/Order.tsx
+++ landpage/src/app/components/Order.tsx
@@ -8,19 +8,22 @@ const glory = Glory({
 
 export default function Order() {
   return (
-    <div className=" my-5 flex flex-col items-center justify-center text-center bg-[#99714B] text-white p-6 sm:p-8 md:p-10 rounded-lg shadow-md w-[90%] max-w-4xl mx-auto mt-6" style={{ fontFamily: glory.style.fontFamily }}>
+    <div
+      className=" my-5 flex flex-col items-center justify-center text-center bg-[#99714B] text-white p-6 sm:p-8 md:p-10 rounded-lg shadow-md w-[90%] max-w-4xl mx-auto mt-6"
+      style={{ fontFamily: glory.style.fontFamily }}
+    >
       <h1 className="text-xl sm:text-2xl md:text-3xl mb-4">
         Faça já a sua encomenda!
       </h1>
       <h2 className="text-lg sm:text-xl md:text-2xl mb-4">
         Entre em contato para realizar o seu pedido:{" "}
         <a
-          href="https://wa.me/5515997853038"
+          href="https://wa.me/5515991768979"
           className="text-[#FFD700] hover:text-[#FFE066] transition-colors"
           target="_blank"
           rel="noopener noreferrer"
         >
-          (15) 99785-3038
+          (15) 99176-8979
         </a>
       </h2>
       <h3 className="text-base sm:text-lg md:text-xl font-light">
